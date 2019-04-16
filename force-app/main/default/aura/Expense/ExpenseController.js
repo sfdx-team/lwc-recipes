88f105e3168248627aba34b930eca72456bdb42d
Gregorio <vasques36@gmail.com>
2019-04-16 18:46:16 -0400

    Expense componenet

--- force-app/main/default/aura/Expense/ExpenseController.js
+++ force-app/main/default/aura/Expense/ExpenseController.js
@@ -1,33 +1,34 @@
 ({
     clickCreate: function(component, event, helper) {
-        var validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
+        var validExpense = component
+            .find('expenseform')
+            .reduce(function(validSoFar, inputCmp) {
+                // Displays error messages for invalid fields
+                inputCmp.showHelpMessageIfInvalid();
+                return validSoFar && inputCmp.get('v.validity').valid;
+            }, true);
         // If we pass error checking, do some real work
-        if(validExpense){
+        if (validExpense) {
             // Create the new expense
-            var newExpense = component.get("v.newExpense");
-            console.log("Create expense: " + JSON.stringify(newExpense));
+            var newExpense = component.get('v.newExpense');
+            console.log('Create expense: ' + JSON.stringify(newExpense));
             helper.createExpense(component, newExpense);
         }
     },
-        // Load expenses from Salesforce
+    // Load expenses from Salesforce
     doInit: function(component, event, helper) {
         // Create the action
-        var action = component.get("c.getExpenses");
+        var action = component.get('c.getExpenses');
         // Add callback behavior for when response is received
         action.setCallback(this, function(response) {
             var state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.expenses", response.getReturnValue());
-            }
-            else {
-                console.log("Failed with state: " + state);
+            if (state === 'SUCCESS') {
+                component.set('v.expenses', response.getReturnValue());
+            } else {
+                console.log('Failed with state: ' + state);
             }
         });
         // Send action off to be executed
         $A.enqueueAction(action);
-    },
-})
+    }
+});
